58310a66d87246879e3b28e11005dd9462ac3e07
RaulMrSouza <raul.mr.souza@gmail.com>
2019-02-21 18:25:18 -0300

    first

---
+++ data.js
@@ -0,0 +1,785 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frente",
+      "name": "Frente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6244381778387815,
+        "pitch": 0.1283714724747398,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8688939171976884,
+          "pitch": 0.2997326768015345,
+          "rotation": 0,
+          "target": "1-entrada"
+        },
+        {
+          "yaw": -2.1695845253985713,
+          "pitch": 0.32478446230165403,
+          "rotation": 0,
+          "target": "5-entrada-p-garagem"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.929971836796561,
+        "pitch": 0.3314104947188685,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1788925922451448,
+          "pitch": 0.2224407916524953,
+          "rotation": 0,
+          "target": "5-entrada-p-garagem"
+        },
+        {
+          "yaw": 2.697510307478926,
+          "pitch": 0.1354315583603718,
+          "rotation": 0,
+          "target": "2-entrada-deck"
+        },
+        {
+          "yaw": -2.5675147402303473,
+          "pitch": -0.09302797145918262,
+          "rotation": 0,
+          "target": "3-deck"
+        },
+        {
+          "yaw": -0.459747617352404,
+          "pitch": 0.05386121359252982,
+          "rotation": 0,
+          "target": "0-frente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-entrada-deck",
+      "name": "Entrada Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4879360008524731,
+        "pitch": -0.015671373227110763,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19697847752540554,
+          "pitch": 0.30352862028754046,
+          "rotation": 0,
+          "target": "1-entrada"
+        },
+        {
+          "yaw": 0.3923642832931744,
+          "pitch": 0.11056033529952103,
+          "rotation": 0,
+          "target": "5-entrada-p-garagem"
+        },
+        {
+          "yaw": -1.6924627851647696,
+          "pitch": 0.05168658958220007,
+          "rotation": 0,
+          "target": "4-abrigo"
+        },
+        {
+          "yaw": -1.2004199012279084,
+          "pitch": 0.08043129124470738,
+          "rotation": 0,
+          "target": "12-espao-gourmet"
+        },
+        {
+          "yaw": -0.7913108384912437,
+          "pitch": 0.1396642028523125,
+          "rotation": 0,
+          "target": "3-deck"
+        },
+        {
+          "yaw": 2.3296780271499262,
+          "pitch": 0.11783551878249021,
+          "rotation": 1.5707963267948966,
+          "target": "14-garagem"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-deck",
+      "name": "Deck",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.9438924414998162,
+        "pitch": 0.2749998396661475,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3074491533549963,
+          "pitch": 0.39409275582736214,
+          "rotation": 0,
+          "target": "1-entrada"
+        },
+        {
+          "yaw": -1.5660836843383592,
+          "pitch": 0.2081222074990805,
+          "rotation": 0,
+          "target": "5-entrada-p-garagem"
+        },
+        {
+          "yaw": -0.08332012651961307,
+          "pitch": 0.363409669234688,
+          "rotation": 0,
+          "target": "2-entrada-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-abrigo",
+      "name": "Abrigo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.166002639600043,
+        "pitch": 0.10545061017053214,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0623885435904974,
+          "pitch": 0.24912789659594026,
+          "rotation": 0,
+          "target": "12-espao-gourmet"
+        },
+        {
+          "yaw": 1.243877005790658,
+          "pitch": 0.31626596153347997,
+          "rotation": 0,
+          "target": "9-sala"
+        },
+        {
+          "yaw": -0.9940590328118866,
+          "pitch": 0.3146786639699819,
+          "rotation": 7.0685834705770345,
+          "target": "2-entrada-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-entrada-p-garagem",
+      "name": "Entrada p/ Garagem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.407745415832327,
+        "pitch": -0.059942051196877344,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5966693210923033,
+          "pitch": 0.10860673393479559,
+          "rotation": 0,
+          "target": "14-garagem"
+        },
+        {
+          "yaw": -0.9243165331775813,
+          "pitch": 0.135789928192402,
+          "rotation": 0,
+          "target": "2-entrada-deck"
+        },
+        {
+          "yaw": 0.8610297761270171,
+          "pitch": 0.27495894904515517,
+          "rotation": 0,
+          "target": "1-entrada"
+        },
+        {
+          "yaw": 2.0560105525477814,
+          "pitch": 0.18574522009684813,
+          "rotation": 0,
+          "target": "0-frente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.807909796483088,
+        "pitch": 0.2959566689607964,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0826945686781606,
+          "pitch": -0.12029782118235843,
+          "rotation": 0,
+          "target": "9-sala"
+        },
+        {
+          "yaw": -1.178275592064093,
+          "pitch": 0.07460507987215692,
+          "rotation": 0,
+          "target": "11-mezanino"
+        },
+        {
+          "yaw": 2.957121057056961,
+          "pitch": 0.1481443984924553,
+          "rotation": 0,
+          "target": "13-fundo"
+        },
+        {
+          "yaw": -0.4534151160457238,
+          "pitch": 0.034580430026867504,
+          "rotation": 0,
+          "target": "8-quarto"
+        },
+        {
+          "yaw": -0.010446970288535695,
+          "pitch": 0.05798758338141141,
+          "rotation": 0,
+          "target": "7-dormitrio"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-dormitrio",
+      "name": "Dormitório",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.814681492597982,
+        "pitch": 0.30046639057704283,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6253323654814729,
+          "pitch": 0.044604428427552634,
+          "rotation": 0,
+          "target": "8-quarto"
+        },
+        {
+          "yaw": -1.4487206837787596,
+          "pitch": -0.07521814339251875,
+          "rotation": 0,
+          "target": "6-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-quarto",
+      "name": "Quarto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3930384919730159,
+        "pitch": 0.18740862024387184,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.158554400564757,
+          "pitch": 0.04133433068951575,
+          "rotation": 0,
+          "target": "7-dormitrio"
+        },
+        {
+          "yaw": -1.1604921851726253,
+          "pitch": 0.0002298386134498287,
+          "rotation": 0,
+          "target": "6-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sala",
+      "name": "Sala",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3855252723865075,
+        "pitch": 0.07193350741652971,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.18829341458579485,
+          "pitch": 0.2711606800667141,
+          "rotation": 0,
+          "target": "4-abrigo"
+        },
+        {
+          "yaw": 1.9895130653154975,
+          "pitch": -0.0555084019092984,
+          "rotation": 0,
+          "target": "6-cozinha"
+        },
+        {
+          "yaw": -3.1327676714604475,
+          "pitch": -0.046838909300527476,
+          "rotation": 0,
+          "target": "10-lareira"
+        },
+        {
+          "yaw": 0.8967397773804411,
+          "pitch": 0.2093304587889122,
+          "rotation": 5.497787143782138,
+          "target": "11-mezanino"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-lareira",
+      "name": "Lareira",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.903696773079453,
+        "pitch": 0.43739352210304716,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7609429429119423,
+          "pitch": 0.38261327717686733,
+          "rotation": 0,
+          "target": "9-sala"
+        },
+        {
+          "yaw": 2.526657195149646,
+          "pitch": 0.2297427587177019,
+          "rotation": 0,
+          "target": "6-cozinha"
+        },
+        {
+          "yaw": 1.1719259978166718,
+          "pitch": 0.14261995316746123,
+          "rotation": 0,
+          "target": "4-abrigo"
+        },
+        {
+          "yaw": 1.777807945253727,
+          "pitch": -0.03783828935814171,
+          "rotation": 0,
+          "target": "11-mezanino"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-mezanino",
+      "name": "Mezanino",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6284390349435203,
+        "pitch": 0.31370213966904004,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4746228147350706,
+          "pitch": 0.5895798266550649,
+          "rotation": 0,
+          "target": "9-sala"
+        },
+        {
+          "yaw": -2.202995705146021,
+          "pitch": 0.2857750625621911,
+          "rotation": 3.9269908169872414,
+          "target": "6-cozinha"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-espao-gourmet",
+      "name": "Espaço Gourmet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5004872301083285,
+        "pitch": 0.42595988593891576,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.94651809187528,
+          "pitch": 0.26515622908528513,
+          "rotation": 0,
+          "target": "4-abrigo"
+        },
+        {
+          "yaw": 0.4359139080435721,
+          "pitch": 0.3149329099072933,
+          "rotation": 0,
+          "target": "2-entrada-deck"
+        },
+        {
+          "yaw": -1.3126855466295666,
+          "pitch": 0.30204777170504826,
+          "rotation": 0,
+          "target": "3-deck"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-fundo",
+      "name": "Fundo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.5039226717986747,
+        "pitch": -0.01950786871569221,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.037572085393773,
+          "pitch": 0.08873974159815745,
+          "rotation": 0,
+          "target": "6-cozinha"
+        },
+        {
+          "yaw": -1.517135342715271,
+          "pitch": 0.10298893093132477,
+          "rotation": 0,
+          "target": "14-garagem"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-garagem",
+      "name": "Garagem",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.9006915749538749,
+        "pitch": 0.008073715216092126,
+        "fov": 1.3333127936580627
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9831994372546404,
+          "pitch": 0.08340161963534598,
+          "rotation": 0,
+          "target": "13-fundo"
+        },
+        {
+          "yaw": -0.4972590951374034,
+          "pitch": 0.03674219917066068,
+          "rotation": 0,
+          "target": "6-cozinha"
+        },
+        {
+          "yaw": 0.6757219178871328,
+          "pitch": 0.18012325815895025,
+          "rotation": 0,
+          "target": "5-entrada-p-garagem"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
